407efc76a928b8643477dbf20cec6cb87ce429c4
Jake McCormick <122489471+jakemcco@users.noreply.github.com>
2023-02-03 12:57:08 -0500

    Add functionality for rounds and games.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -23,7 +23,7 @@ function getPlayerChoice() {
 
     //Keep prompting until we get input
     if (userInput === null || userInput === "") { 
-        getPlayerChoice();
+        return getPlayerChoice();
     }
     //Typecast input to string, convert to  lower-case, confirm valid input
     else {
@@ -32,30 +32,112 @@ function getPlayerChoice() {
         switch (cleanedUserInput) {
             case 'rock':
                 playerChoice = 'rock';
+                return playerChoice;
                 break;
             case 'paper':
                 playerChoice = 'paper';
+                return playerChoice;
                 break;
             case 'scissors':
                 playerChoice = 'scissors';
+                return playerChoice;
                 break;
             default:
                 alert('Invalid input, please enter: Rock, Paper, or Scissors');
-                getPlayerChoice();
+                return getPlayerChoice();
         }
-
-        return playerChoice;
     }
 }
 
+// Returns 0 if tie, 1 if computer wins, 2 if player wins
+function playRound(computerSelection = getComputerChoice(), playerSelection = getPlayerChoice()) {
+    console.log('---------- Round start ----------');
+    console.log('Computer has selected: '+ computerSelection);
+    console.log('Player has selected: '+ playerSelection);
 
-const computerSelection = getComputerChoice();
-console.log('Computer has selected: '+ computerSelection);
+    //First test for tie
+    if (computerSelection === playerSelection) {
+        console.log('Round result: tie');
+        return 0;
+    }
+    
+    //Then if it's not a tie, test variations for winner
+    switch (computerSelection) {
+        case 'rock':
+            if (playerSelection === 'paper'){
+                console.log('Round result: Player Wins');
+                return 2;
+            }
+            else if (playerSelection === 'scissors'){
+                console.log('Round result: Computer Wins');
+                return 1;
+            }
+            break;
 
-const playerSelection = getPlayerChoice();
-console.log('Player has selected: '+ playerSelection);
+        case 'paper':
+            if (playerSelection === 'rock'){
+                console.log('Round result: Computer Wins');
+                return 1;
+            }
+            else if (playerSelection === 'scissors'){
+                console.log('Round result: Player Wins');
+                return 2;
+            }
+            break;
+        
+        case 'scissors':
+            if (playerSelection === 'paper'){
+                console.log('Round result: Computer Wins');
+                return 1;
+            }
+            else if (playerSelection === 'rock'){
+                console.log('Round result: Player Wins');
+                return 2;
+            }
+            break;
+    return;
+    }
 
-
-function playRound(playerSelection, computerSelection) {
 
 }
+
+
+function game() {
+    console.log('***** Starting game *****');
+    let computerScore = 0;
+    let playerScore = 0;
+    let gameResult;
+
+    for (let i = 0; i < 5; i++) {
+        let roundWinner = playRound();
+        switch (roundWinner){
+            case 0:
+                break;
+            case 1:
+                computerScore++;
+                break;
+            case 2:
+                playerScore++;
+                break;
+        }
+        console.log(`Score is --- Computer: ${computerScore}  Player: ${playerScore}`);
+    }
+
+    if (!(computerScore === playerScore)) {
+        gameResult = (computerScore > playerScore) ? "Computer Wins!" : "Player Wins!";
+    }
+    else{
+        gameResult = "Draw";
+    }
+  
+    console.log(`Final Score is --- Computer: ${computerScore}  Player: ${playerScore}. Game result: ${gameResult}`);
+}
+
+game();
+
+
+// const computerSelection = getComputerChoice();
+// console.log('Computer has selected: '+ computerSelection);
+
+// const playerSelection = getPlayerChoice();
+// console.log('Player has selected: '+ playerSelection);
