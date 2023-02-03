ad47c3fa16f3b2e9d4719cebff27253633db3be2
Jake McCormick <122489471+jakemcco@users.noreply.github.com>
2023-02-03 11:44:40 -0500

    Create getPlayerChoice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,5 @@
-console.log('Test Test Test');
 
-function getComputerChoice(){
+function getComputerChoice() {
     const choice = Math.floor(Math.random() * 3); //Returns 0, 1, or 2
     
     switch (choice){
@@ -16,4 +15,47 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice());
+
+function getPlayerChoice() {
+    const userInput = prompt('Please choose: Rock, Paper, or Scissors');
+    let cleanedUserInput;
+    let playerChoice;
+
+    //Keep prompting until we get input
+    if (userInput === null || userInput === "") { 
+        getPlayerChoice();
+    }
+    //Typecast input to string, convert to  lower-case, confirm valid input
+    else {
+        cleanedUserInput = (userInput.toString()).toLowerCase();
+
+        switch (cleanedUserInput) {
+            case 'rock':
+                playerChoice = 'rock';
+                break;
+            case 'paper':
+                playerChoice = 'paper';
+                break;
+            case 'scissors':
+                playerChoice = 'scissors';
+                break;
+            default:
+                alert('Invalid input, please enter: Rock, Paper, or Scissors');
+                getPlayerChoice();
+        }
+
+        return playerChoice;
+    }
+}
+
+
+const computerSelection = getComputerChoice();
+console.log('Computer has selected: '+ computerSelection);
+
+const playerSelection = getPlayerChoice();
+console.log('Player has selected: '+ playerSelection);
+
+
+function playRound(playerSelection, computerSelection) {
+
+}
